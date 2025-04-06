78db97ebaef2619a34d6e1495994cb1e7251f415
Sreekumr <abhisheksreehari4@gmail.com>
2025-04-06 17:44:55 +0530

    First commit on .net sample project

---
+++ NewPaymentAPI/Models/PayementDetail.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NewPaymentAPI.Models
+{
+    public class PayementDetail
+    {
+        [Key]
+        public int PayementId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string CardOwerName { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(16)")]
+        public string CardNumber { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExpirationDate { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(3)")]
+        public string SecurityCode { get; set; } = "";
+    }
+}
---
+++ NewPaymentAPI/Models/PaymentDetailContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NewPaymentAPI.Models
+{
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<PayementDetail> PayementDetails { get; set; }
+    }
+}
